955d85e0bfe32225daf85b5a6ddae61627c1c92d
Maria <369aksi@gmail.com>
2021-10-20 16:55:37 +0300

    The second task done

--- src/main/java/ru/gb/m/str/HW2.java
+++ src/main/java/ru/gb/m/str/HW2.java
@@ -3,7 +3,10 @@ package ru.gb.m.str;
 public class HW2 {
     public static void main(String[] args) {
         sumOfNumber();
+        number ();
     }
+
+
 
     public static boolean sumOfNumber() {
         int a = 8;
@@ -15,5 +18,13 @@ public class HW2 {
             return false;
         }
     }
-}
 
+    public static void number() {
+        int a = 0;
+        if (a >= 0) {
+            System.out.println("Число а положительное");
+        } else {
+            System.out.println("Число а отрицательное");
+        }
+    }
+}
